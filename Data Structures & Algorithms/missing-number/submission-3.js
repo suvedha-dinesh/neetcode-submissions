3ea7c7e27bcfd4564d8c2a38c98af54ceebf92c4
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-04-08 13:18:34 +0530

    Add: missing-number - submission-3

---
+++ Data Structures & Algorithms/missing-number/submission-3.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    missingNumber(nums) {
+        nums.sort((a, b) => a-b);
+        for(let i=0; i<nums.length; i++){
+            if(nums[i] !== i){
+                return i
+            }
+        }
+        return nums.length;
+    }
+}
