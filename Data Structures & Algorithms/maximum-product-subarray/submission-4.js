4cf85743057fc97a1f0d256ba6b7bc916885b67c
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-05-12 16:14:21 +0530

    Add: maximum-product-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-4.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxProduct(nums) {
+        let max = nums[0];
+    let maxProd = nums[0];
+    let minProd= nums[0];
+        for(let i=1; i< nums.length; i++){
+           [maxProd, minProd] = [Math.max(nums[i] , minProd * nums[i], maxProd * nums[i])
+            , Math.min(nums[i] , minProd * nums[i], maxProd * nums[i])];
+            max = Math.max(max, maxProd);
+        }
+
+return max;
+
+
+
+
+
+
+
+
+
+        //     let max = -Infinity;
+        // for(let i=0; i<nums.length;i++){
+        //    let currMul = nums[i];
+        //     max = Math.max(currMul, max)
+        //     for(let j=i+1; j<nums.length;j++){
+        //          currMul *= nums[j];
+        //         max = Math.max(currMul, max)
+        //     }
+        // }
+        // return max;
+    }
+}
