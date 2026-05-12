f6bbcce13f8efbc31979323a3b6af8535f2a9c8f
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-05-12 16:19:00 +0530

    Add: maximum-product-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-5.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxProduct(nums) {
+        let max = -Infinity;
+        for(let i=0; i< nums.length; i++){
+            let temp = nums[i];
+            for(let j=i; j<nums.length; j++){
+                if( i < j){
+                    temp *= nums[j]
+                }
+                max = Math.max(max, temp)
+            }
+        }
+        return max;
+    }
+}
