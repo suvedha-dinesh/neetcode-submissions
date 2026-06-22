2352ea45cac08d1992bfff0da1ca22cb098af883
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-06-22 15:44:31 +0530

    Add: count-paths - submission-5

---
+++ Data Structures & Algorithms/count-paths/submission-5.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m, n) {
+            let dp = new Array(m).fill(-1).map(() => new Array(n).fill(-1))
+        function dfs(i, j){
+            if( i>=m || j >= n) return 0;
+            if(i === m-1 && j === n-1) return 1;
+            if(dp[i][j] !== -1){
+                return dp[i][j]
+            }
+            dp[i][j] = dfs(i+1, j) + dfs(i, j+1);
+            return dp[i][j]
+        }
+      return  dfs(0, 0);
+    }
+}
