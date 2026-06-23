17a4ab9512edb7ae94dcd31a4dbb9b0409d3c7b6
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-06-23 23:18:35 +0530

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    maxAreaOfIsland(grid) {
+        let m = grid.length;
+        let n = grid[0].length;
+        let noOfIslands = 0;
+        let max =0;
+
+        function dfs(r, c){
+            if( r <0 || c <0 || r >= m || c >= n || grid[r][c] === 0) return;
+            noOfIslands++;
+            grid[r][c] = 0;
+            dfs(r+1, c);
+            dfs(r-1, c);
+            dfs(r, c-1);
+            dfs(r, c+1);
+        }
+
+
+
+        for(let i =0;i <m; i++){
+            for(let j=0; j<n; j++){
+                if(grid[i][j] === 1){
+                    dfs(i, j);
+                    max = Math.max(max, noOfIslands)
+                    noOfIslands = 0;
+                }
+                
+            }
+        }
+       return max;
+    }
+    
+    
+}
