d9d4c44236a824566bf18d9e5e0c255dd4722f96
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-07-14 15:15:51 +0530

    Add: count-number-of-islands - submission-5

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-5.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+    numIslands(grid) {
+        let m = grid.length; 
+        let n = grid[0].length;
+        let max = 0;
+        let isLands =0;
+        function dfs(r, c){
+            if( r>=m || c >=n || r< 0 || c<0 || grid[r][c] === '0') return;
+            grid[r][c] ='0';
+            dfs(r+1, c);
+            dfs(r, c-1);
+            dfs(r, c+1);
+            dfs(r-1, c);
+        }
+
+        for(let r=0; r< m;r++){
+            for(let c=0; c<n; c++){
+                if(grid[r][c] === '1'){
+                    dfs(r, c);
+                    isLands++;
+                }
+            }
+        }
+        return isLands;
+    }
+}
