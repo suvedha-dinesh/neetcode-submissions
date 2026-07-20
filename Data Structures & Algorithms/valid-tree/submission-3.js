e6d935d53b1c5904f3e6b0c8b96efa15eb9c1589
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-07-20 20:42:00 +0530

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    // 0 [1,2,3]  []
+    // 1 [4]     [0]
+    // 2         [0]
+    // 3         [0]
+    // 4         [1]
+
+    validTree(n, edges) {
+        let adj = new Map();
+        for(let i=0; i<n; i++){
+            adj.set(i, [])
+        }
+        for(let [n1, n2] of edges){
+            adj.get(n1).push(n2); 
+            adj.get(n2).push(n1);
+        }
+        let visited = new Set();
+        function dfs(i, prev){
+            if(visited.has(i)) return false;
+            visited.add(i)
+            for(let j of adj.get(i)){
+                if(j === prev) continue;
+                if(!dfs(j, i)) return false;
+            }
+            return true;
+        }
+        return dfs(0, -1) && visited.size === n;
+    }
+}
