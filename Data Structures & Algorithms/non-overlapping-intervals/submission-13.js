804985ebb98551250f8284dde72d2dc5ec9c75db
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-07-21 21:34:36 +0530

    Add: non-overlapping-intervals - submission-13

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-13.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals) {
+        intervals.sort((a, b) => a[1] - b[1]);
+        let count= 0;
+        let prev = intervals[0];
+        for(let i=1; i< intervals.length; i++){
+            if(prev[1] > intervals[i][0]){
+                count++;
+            } else prev[1] = intervals[i][1];
+        }
+        return count;
+    }
+}
