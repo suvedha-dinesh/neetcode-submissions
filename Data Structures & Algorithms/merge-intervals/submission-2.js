ec577cd0612c40499f86f27060a9035f649cd034
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-07-21 20:56:18 +0530

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        if(intervals.length === 1) return intervals;
+        intervals.sort((a, b) => a[0] - b[0])
+        let result= [intervals[0]];
+        
+        for(let i=1; i<intervals.length; i++){
+            let prev = result[result.length -1];
+            let current = intervals[i];
+
+            if(current[0] <= prev[1]){
+                prev[1] = Math.max(prev[1], current[1])
+            } else result.push(current)
+        }
+        return result;
+
+    }
+}
