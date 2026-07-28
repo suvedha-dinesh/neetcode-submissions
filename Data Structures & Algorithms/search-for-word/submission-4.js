2bb636d07b7856bcdefc62534b6169dd96ff3774
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-07-28 13:37:01 +0530

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @param {string} word
+     * @return {boolean}
+     */
+    exist(board, word) {
+        let m = board.length;
+        let n = board[0].length;
+
+      function dfs(r, c, i){
+        if(i=== word.length) return true;
+        if( r <0 || c<0 || r >= m || c>= n || word[i] !== board[r][c]) return false;
+        let visited = board[r][c];
+        board[r][c] = '#';
+        const found = dfs(r, c+1, i+1) || dfs(r, c-1, i+1) || dfs(r+1, c, i+1) || dfs(r-1, c, i+1);
+        board[r][c] = visited;
+        return found;
+      }
+        for(let r=0; r <m; r++){
+            for(let c=0; c<n;c++){
+                if(dfs(r, c, 0)) return true;
+            }
+        }
+        return false;
+    }
+}
