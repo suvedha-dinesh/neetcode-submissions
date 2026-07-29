2a233f052c0f09adfcd1b35c03f5df6ceaadd83d
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-07-29 21:26:58 +0530

    Add: foreign-dictionary - submission-0

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-0.js
@@ -0,0 +1,67 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @returns {string}
+     */
+    foreignDictionary(words) {
+        let adj= new Map();
+
+        for(let word of words){
+            for(let char of word){
+                if(!adj.has(char)) adj.set(char, []);
+            }
+            /* 
+            adj = {
+    w: [],
+    r: [],
+    t: [],
+    f: [],
+    e: []
+}
+            */
+        }
+
+        for(let i=0; i<words.length -1; i++){
+            let w1 = words[i];
+            let w2 = words[i+1];
+            let len = Math.min(w1.length, w2.length);
+            if(w1.length > w2.length && w1.startsWith(w2)) return '';
+            for(let j=0; j<len;j++){
+                if(w1[j] !== w2[j]){
+                    /*
+                    adj = {
+    w: ['e'],
+    e: ['r'],
+    r: ['t'],
+    t: ['f'],
+    f: []
+}
+                     */
+                    adj.get(w1[j]).push(w2[j]);
+                    break;
+                }
+            }
+        }
+        let visiting = new Set();
+        let visited = new Set();
+        let result= [];
+        function dfs(ch){
+            if(visiting.has(ch)) return false;//found the cycle check notes for example
+            if(visited.has(ch)) return true; //already processed
+            visiting.add(ch);
+            for(let j of adj.get(ch)){
+                if(!dfs(j)) return false; //false means "a cycle exists in the directed graph."
+            }
+            visiting.delete(ch);
+            visited.add(ch);
+            result.push(ch);
+            return true;
+        }
+
+        for(let ch of adj.keys()){
+            if(!dfs(ch)) return '';
+        }
+        return result.reverse().join('');
+
+    }
+}
