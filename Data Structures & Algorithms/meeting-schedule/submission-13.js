88fc40a5da3c46b1d0621eb48be61114283af053
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-07-29 18:00:15 +0530

    Add: meeting-schedule - submission-13

---
+++ Data Structures & Algorithms/meeting-schedule/submission-13.js
@@ -0,0 +1,30 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+        
+        if(intervals.length === 0) return true;
+        intervals.sort((a, b) => a.start - b.start);
+        let prev = intervals[0];
+        for(let i=1; i<intervals.length; i++){
+            if(prev.end > intervals[i].start){
+                return false;
+            } else {
+                prev.end = Math.max(prev.end, intervals[i].end)
+            }
+        }
+        return true;
+    }
+}
