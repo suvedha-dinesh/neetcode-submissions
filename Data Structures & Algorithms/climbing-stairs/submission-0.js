6a6c432f96c2a8de0a5fb58bb2606e9794428513
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-07-29 22:25:18 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        const noOfWays = [1,2];
+        for(let i=2; i<n; i++){
+            noOfWays[i] = noOfWays[i-1] + noOfWays[i-2];
+        }
+        return noOfWays[n-1];
+    }
+}
