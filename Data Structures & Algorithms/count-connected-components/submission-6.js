3a4df7764dffbce35c42e3b5652bdd6d2282b982
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-08-30 22:51:02 +0530

    Add: count-connected-components - submission-6

---
+++ Data Structures & Algorithms/count-connected-components/submission-6.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     * 0 [1]
+     * 1 [2, 0]
+     * 2 [1]
+     * 3 [4]
+     * 4 [3]
+     */
+    countComponents(n, edges) {
+        let adj = new Map();
+        for(let i=0; i<n; i++){
+            adj.set(i, []);
+        }
+
+        for(let [n1, n2] of edges){
+            adj.get(n1).push(n2);
+            adj.get(n2).push(n1);
+        }
+        let count =0;
+        let visited = new Set();
+        function dfs(n){
+            if(visited.has(n)) return;
+            visited.add(n);
+            for(let j of adj.get(n)){
+                dfs(j);
+            }
+        }
+
+        for(let i=0; i<n; i++){
+            if(!visited.has(i)){
+            dfs(i);
+            count++;
+            }
+        }
+        return count;
+    }
+    
+}
