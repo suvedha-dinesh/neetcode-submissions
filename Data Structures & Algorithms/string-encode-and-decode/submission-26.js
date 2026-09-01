441580d886181f2b55148ca58ae3b6cd6c5e330a
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-09-01 17:42:46 +0530

    Add: string-encode-and-decode - submission-26

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-26.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let result = '';
+        for(let str of strs){
+            result += `${str.length}#${str}`
+        }
+        return result;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    //4#NEET
+    decode(str) {
+        let i=0;
+        let result= [];
+
+        while(i < str.length){
+            let pos = str.indexOf('#', i); //1
+            let len = parseInt(str.slice(i, pos)); // 4
+            i= pos+1; //2
+            let string = str.slice(i, len+i); //NEET
+            result.push(string);
+            i+=len;
+        }
+        return result;
+    }
+}
