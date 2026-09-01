58d92fae5cbe0892000797ed917e8340f222fa3d
Suvedha Dinesh <97978420+suvedha-dinesh@users.noreply.github.com>
2026-09-01 18:09:59 +0530

    Add: longest-consecutive-sequence - submission-27

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-27.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        nums.sort((a, b) => a-b);
+        if(nums.length === 0) return 0;
+        let max =1;
+        let count=1;
+        for(let i=0; i< nums.length; i++){
+            if(nums[i] === nums[i+1]){
+                continue;
+            } else if(nums[i]+1 === nums[i+1]){
+                count++;
+                max = Math.max(max, count)
+            } else count=1;
+        }
+
+        return max;
+    }
+}
